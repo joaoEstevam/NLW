a20848596a82fa4d25d03900a6c2699101ab4539
João Estevam <joaoestevam823@gmail.com>
2024-09-11 15:35:41 -0300

    Cadastrar metas

---
+++ index.js
@@ -0,0 +1,59 @@
+const { select, input } = require('@inquirer/prompts')
+
+let meta = {
+    value: "Tomar 3L de agua todos os dias",
+    checked: false
+}
+
+let metas =[meta]
+
+
+const cadastrarMeta = async () => {
+    const meta = await input({message: "Digite a meta:"})
+
+    if(meta.length == 0){
+        console.log("A meta não pode ser vazia")
+        return
+    }
+
+    metas.push(
+        {value: meta,checked:false}
+        )
+}
+
+const start = async () => {
+
+    while(true){
+
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    names: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value:"sair"
+                }
+            ]
+        })
+
+        switch(opcao){
+            case "cadastrar":
+                await cadastrarMeta()
+                console.log(metas)
+                break
+            case "listar":
+                console.log("vamos listar")
+                break
+            case "sair":
+                return
+        }
+    }
+}
+start()
